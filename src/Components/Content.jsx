f1f949dbfbc5af9644391ba8f980929ca9be843d
oZep <97713154+oZep@users.noreply.github.com>
2024-03-07 04:40:57 +0000

    content done

---
+++ src/Components/Content.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <main className="content" id="content" data-scroll="area">
+      <div className="content_inner" data-scroll="target">
+        {/* Your other components go here */}
+      </div>
+    </main>
+  );
+};
+
+export default Content;
