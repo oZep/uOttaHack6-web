6f38bb33261034c10711e9d717d3a9aac67372ad
oZep <97713154+oZep@users.noreply.github.com>
2024-03-08 06:34:03 +0000

    tired

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import { useStore } from "react-admin";
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   const $store = useStore(); // Define the $store variable using the imported store module
@@ -12,24 +12,34 @@ const Home = () => {
       <nav className="siteHeader_nav">
         <ol>
           <li className={`${currentPath === "/" ? "is-selected" : ""}`}>
-            { currentPath === "/" ? <div className="_dot">●</div> : null }
-            <a className="_text" href="/">Home</a>
+            {currentPath === "/" ? <div className="_dot">●</div> : null}
+            <a className="_text" href="/">
+              Home
+            </a>
           </li>
-          <li className={`${currentPath === "/projects/" ? "is-selected" : ""}`}>
-            { currentPath === "/projects/" ? <div className="_dot">●</div> : null }
-            <a className="_text" href="/projects/">Projects</a>
+          <li className={`${currentPath === "/projects" ? "is-selected" : ""}`}>
+            {currentPath === "/projects" ? <div className="_dot">●</div> : null}
+            <a className="_text" href="/projects">
+              Projects
+            </a>
           </li>
-          <li className={`${currentPath === "/info/" ? "is-selected" : ""}`}>
-            { currentPath === "/info/" ? <div className="_dot">●</div> : null }
-            <a className="_text" href="/info/">Info</a>
+          <li className={`${currentPath === "/info" ? "is-selected" : ""}`}>
+            {currentPath === "/info" ? <div className="_dot">●</div> : null}
+            <a className="_text" href="/info">
+              Info
+            </a>
           </li>
-          <li className={`${currentPath === "/contact/" ? "is-selected" : ""}`}>
-            { currentPath === "/contact/" ? <div className="_dot">●</div> : null }
-            <a className="_text" href="/contact/">Contact</a>
+          <li className={`${currentPath === "/contact" ? "is-selected" : ""}`}>
+            {currentPath === "/contact" ? <div className="_dot">●</div> : null}
+            <a className="_text" href="/contact">
+              Contact
+            </a>
           </li>
-          <li className={`${currentPath === "/faq/" ? "is-selected" : ""}`}>
-            { currentPath === "/faq/" ? <div className="_dot">●</div> : null }
-            <a className="_text" href="/faq/">FAQ</a>
+          <li className={`${currentPath === "/faq" ? "is-selected" : ""}`}>
+            {currentPath === "/faq" ? <div className="_dot">●</div> : null}
+            <a className="_text" href="/faq">
+              FAQ
+            </a>
           </li>
         </ol>
       </nav>
